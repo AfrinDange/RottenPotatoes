87ae28386573b5bedfeb9feb235c449f630a184c
AfrinDange <dangeafrin@gmail.com>
2020-11-26 01:47:28 +0530

    Added user profile - displays user details and review written

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,31 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $userID = Auth::id();
+        $userdetails = DB::select('SELECT name, avatar, username, email
+                                    FROM userdetails
+                                    WHERE id = ?', [$userID]);
+
+        $moviesReview = DB::select('SELECT moviesreview.title as reviewTitle, content, rating, movieimdbID, movies.title as movieTitle, poster
+                                    FROM moviesreview, movies
+                                    WHERE movieimdbID = imdbID AND userID = ?', [$userID]);
+
+        $tvseriesReview = DB::select('SELECT tvseriesreview.title as reviewTitle, content, rating, tvseriesimdbID, tvseries.title as showTitle, poster
+                                    FROM tvseriesreview, tvseries
+                                    WHERE tvseriesimdbID = imdbID AND userID = ?', [$userID]);
+        foreach ($moviesReview as $review) {
+            //encoded to dataURL format to add to the html tag
+            $dataURI = (string) base64_encode($review->poster);
+            $review->poster = $dataURI;
+        }
+        foreach ($tvseriesReview as $review) {
+            //encoded to dataURL format to add to the html tag
+            $dataURI = (string) base64_encode($review->poster);
+            $review->poster = $dataURI;
+        }
+        return view('home')
+                    ->with('userdetails', $userdetails)
+                    ->with('moviesReview', $moviesReview)
+                    ->with('tvseriesReview', $tvseriesReview);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,93 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="p-3 container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header bg-dark lead text-white">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
+                <div class="card-body bg-secondary text-white">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-                    {{ __('You are logged in!') }}
-                    <div class="row d-flex">
+                    <div class="row align-items-end">
                         <div class="col-3">
-                            <img src="/images/avatar1.png" alt="" width = 150 height = 150 alt="" style = "margin: auto;"
-                             class="d-flex justify-content-center bg-primary img-fluid img-thumbnail rounded-circle">
+                            <img src="{{ '/images/avatar'.$userdetails[0]->avatar.'.png' }}" alt="" width = 150 height = 150 alt="" style = "margin: auto;"
+                             class="d-flex justify-content-center img-fluid img-thumbnail rounded-circle">
+                        </div>
+                        <div class="col-9">
+                            <div><h2>{{ $userdetails[0]->name }}</h2></div>
+                            <div><p class="p-0 m-0">Username: {{ $userdetails[0]->username}}</p></div>
+                            <div><p class="p-0 m-0">Email ID: {{ $userdetails[0]->email }}</p></div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="accordion w-75 pt-3" id="reviewsection"> 
+            @foreach ($moviesReview as $review)
+            <div class="card bg-transparent pb-3">
+                <div class="card-header p-0" id="title">
+                    <div class = "row no-gutters">
+                        <div class="col-lg-2 col-md-4">
+                                <img src="data:image/png;base64, {{ $review->poster }}"
+                                    class="card-img border border-dark" alt="...">
+                        </div>
+                        <div class="pl-5 pt-3 pr-3 col-lg-10 col-md-8 d-flex">
+                            <div class = "d-flex flex-column justify-content-start">
+                                <p style="font-size: 1.5rem">Review For <a class="text-decoration-none" href="/movies/{{ $review->movieimdbID }}">{{ $review->movieTitle}}</a></p>
+                                <h4 class="lead">{{ $review->reviewTitle}}</h4>
+                                <div class="d-flex">
+                                    <label class="font-weight-bolder" for="rating">Ratings:</label>
+                                    <div id="rating" class="progress bg-light m-1" style="width: 200px">
+                                        <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="{{ 'width: '.$review->rating.'%' }}" aria-valuenow="{{$review->rating }}" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                                <button class="btn btn-link text-left m-0 p-0" type="button" data-toggle="collapse" data-target="{{ '#reviewby'.$review->movieimdbID }}" aria-expanded="true" aria-controls="{{ 'reviewby'.$review->movieimdbID }}">
+                                read more . . . 
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="{{ 'reviewby'.$review->movieimdbID }}" class="collapse" aria-labelledby="title" data-parent="#reviewsection">
+                    <div class="card-body">{{ $review->content }}</div>
+                </div>
+            </div>
+            @endforeach
+            @foreach ($tvseriesReview as $review)
+            <div class="card bg-transparent pb-3">
+                <div class="card-header p-0" id="title">
+                    <div class = "row no-gutters">
+                        <div class="col-lg-2 col-md-4">
+                                <img src="data:image/png;base64, {{ $review->poster }}"
+                                    class="card-img" alt="...">
+                        </div>
+                        <div class="pl-5 pt-3 pr-3 col-lg-10 col-md-8 d-flex">
+                            <div class = "d-flex flex-column justify-content-start">
+                                <p style="font-size: 1.5rem">Review For <a class="text-decoration-none" href="/tvseries/{{ $review->tvseriesimdbID }}">{{ $review->showTitle}}</a></p>
+                                <h4 class="lead">{{ $review->reviewTitle}}</h4>
+                                <div class="d-flex">
+                                    <label class="font-weight-bolder" for="rating">Ratings:</label>
+                                    <div id="rating" class="progress bg-light m-1" style="width: 200px">
+                                        <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="{{ 'width: '.$review->rating.'%' }}" aria-valuenow="{{$review->rating }}" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                                <button class="btn btn-link text-left m-0 p-0" type="button" data-toggle="collapse" data-target="{{ '#reviewby'.$review->tvseriesimdbID }}" aria-expanded="true" aria-controls="{{ 'reviewby'.$review->tvseriesimdbID }}">
+                                read more . . . 
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="{{ 'reviewby'.$review->tvseriesimdbID }}" class="collapse" aria-labelledby="title" data-parent="#reviewsection">
+                    <div class="card-body">{{ $review->content }}</div>
+                </div>
+            </div>
+            @endforeach
         </div>
     </div>
 </div>
